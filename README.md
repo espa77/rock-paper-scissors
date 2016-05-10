681d4b2382b9eabc6902d083c6b088947d926b5d
espa77 <ethansteiner@gmail.com>
2016-05-10 12:46:12 -0400

    added comments, readme, and moved messaging components to same location

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # rock-paper-scissors
+
+Rock Paper Scissors
+
+This is a Rock Paper Scissors game. The game was written given this user input example:
+
+>$ruby rps.rb favorite
+You are playing against strategy 'favorite'.
+Type 'r', 'p' or 's'.
+>r
+I chose 's'. You win!
+you won 1 times.
+you lost 0 times.
+we tied 0 times.
+Type 'r', 'p' or 's'.
+>p
+I chose 'p'. It's a tie!
+you won 1 times.
+you lost 0 times.
+we tied 1 times.
+>
+
+To start the game follow these instructions:
+1. Use Ruby 2.3
+2. Download the repo
+3. ruby rps.rb
+
+To exit the game type 'exit'
+
+Computer Strategy 1: Beat Their Last Move.
+
+Here you will play the move that beats their last move.
+
+Computer Strategy 2: Beat Their Favorite.
+
+Here you will play the move that beats their most frequent move.
+
+Computer Strategy 3: Extra Credit. Random moves
+
+Randomized computer moves to keep you guessing!
+
+BONUS - When the game is started the computer randomly selects a strategy from the 3 above and implements it throughout the time the game is played. This challenges the player to figure out the strategy first. Then focus on winning the game!
+
+The program will announce the strategy to the user upon entering exiting the program.
+
+Future plans = This was made in approximately 4 hours. If I had more time, I would re-factor some of the methods to remove some of the smelly code. I would also segment the implementation of the player input, game play logic, and messaging into separate classes for easier additions/building. 
+
+All code written by Ethan Steiner, May 10, 2016.
+
+This code is open-source. Feel free to use it - or any part of it as you wish. Please give credit if you use some or all of it in your project.
--- rps.rb
+++ rps.rb
@@ -1,14 +1,15 @@
-# Rock Paper Scissors - Ethan Steiner
+# Rock Paper Scissors - Ethan Steiner May, 10, 2016 (347)873-1681
 
 class Game
   attr_accessor :player_input, :player_choice, :strategy
 
+  # rules for game play
   RULES = {
     :rock     => {:rock => :draw, :paper => :paper, :scissors => :rock},
     :paper    => {:rock => :paper, :paper => :draw, :scissors => :scissors},
     :scissors => {:rock => :rock, :paper => :scissors, :scissors => :draw}
   }
-
+  # keeps log of players choices for the lifecycle of one game
   @@player_choice_log = {rock: 0, paper: 0, scissors: 0}
 
   def initialize
@@ -22,50 +23,38 @@ class Game
     @player_input = player_input
   end
 
+  # handles all player input and player choice logging
   def player_input
     loop do
       puts "Type 'r', 'p' or 's'.\n"
-
       computer_choice = strategy_implement(strategy)
       input = gets.chomp.downcase
       if input == 'r'
         value = {rock: 1}
         player_choice.clear
         player_choice.merge!(value)
-        puts "Rock"
-        # puts "I chose 'r'. We tied"
+        puts "Rock\n"
         play_game(value.keys.pop, computer_choice)
         @previous_player_choice = value
         add_player_choice_to_log
-
       elsif input == 'p'
         value = {paper: 1}
         player_choice.clear
         player_choice.merge!(value)
-        puts "Paper"
-        # puts "I chose 's'. You lose"
+        puts "Paper\n"
         play_game(value.keys.pop, computer_choice)
         add_player_choice_to_log
         @previous_player_choice = value
-
       elsif input == 's'
         value = {scissors: 1}
         player_choice.clear
         player_choice.merge!(value)
-        puts "Scissors"
-        # puts "I chose 'p'. You win"
+        puts "Scissors\n"
         play_game(value.keys.pop, computer_choice)
         add_player_choice_to_log
         @previous_player_choice = value
-
       elsif input == 'exit'
-        if strategy == 1
-          puts "Thank you for playing against the Beat Last Move computer strategy"
-        elsif strategy == 2
-          puts "Thank you for playing against the Beat Player Favorite computer strategy"
-        else
-          puts "Thank you for playing against the Random computer strategy"
-        end
+        exit_message
         break
       else
         puts "That is not a valid choice - please try again"
@@ -73,16 +62,20 @@ class Game
     end
   end
 
+  # game play logic and tracking win/loss/ties
   def play_game(human, computer)
     winner = RULES[human][computer]
     if winner == human
       @won+=1
+      puts game_complete_message(computer) + "You win!"
     elsif winner == computer
       @lost+=1
+      puts game_complete_message(computer) + "You lose."
     else
       @tie+=1
+      puts game_complete_message(computer) + "It's a tie."
     end
-    puts "you won #{@won} times.\nyou lost #{@lost} times.\nwe tied #{@tie} times."
+    puts "you won #{@won} times.\nyou lost #{@lost} times.\nwe tied #{@tie} times.\n\n"
   end
 
   def add_player_choice_to_log
@@ -91,16 +84,13 @@ class Game
     @@player_choice_log[choice] = new_value
   end
 
-  def welcome_message
-    puts "\n"
-    puts "Welcome to Rock Paper Scissors"
-  end
-
+  # computer strategy picker
   def select_strategy
     strategy_picker = [1, 2, 3].sample
     return strategy_picker
   end
 
+  # computer strategy implementer
   def strategy_implement(strategy_picker)
     if strategy_picker == 1
       computer_strategy = beat_last_move
@@ -111,6 +101,7 @@ class Game
     end
   end
 
+  # computer strategies
   def beat_last_move
     if @previous_player_choice == {}
       computer_choice = [:rock, :paper, :scissors].sample
@@ -143,6 +134,32 @@ class Game
   def random_computer
     computer_choice = [:rock, :paper, :scissors].sample
     return computer_choice
+  end
+
+  # messaging for start, play, and exit
+  def welcome_message
+    puts "\n"
+    puts "Welcome to Rock Paper Scissors"
+  end
+
+  def game_complete_message(computer_choice)
+    if computer_choice == :rock
+      "I chose 'r'. "
+    elsif computer_choice == :paper
+      "I chose 'p'. "
+    else
+      "I chose 's'. "
+    end
+  end
+
+  def exit_message
+    if strategy == 1
+      puts "\nThank you for playing against the Beat Last Move computer strategy\n"
+    elsif strategy == 2
+      puts "\nThank you for playing against the Beat Player Favorite computer strategy\n"
+    else
+      puts "\nThank you for playing against the Random computer strategy\n"
+    end
   end
 
 end
