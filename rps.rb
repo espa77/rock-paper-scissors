75f1c84083cffdb992908175d08a280cf4474a8b
espa77 <ethansteiner@gmail.com>
2016-05-10 11:57:31 -0400

    finished full computer player functionality

--- rps.rb
+++ rps.rb
@@ -1,54 +1,71 @@
 # Rock Paper Scissors - Ethan Steiner
-require 'pry'
+
 class Game
-  attr_accessor :player_input, :player_choice
+  attr_accessor :player_input, :player_choice, :strategy
+
   RULES = {
     :rock     => {:rock => :draw, :paper => :paper, :scissors => :rock},
     :paper    => {:rock => :paper, :paper => :draw, :scissors => :scissors},
     :scissors => {:rock => :rock, :paper => :scissors, :scissors => :draw}
   }
 
+  @@player_choice_log = {rock: 0, paper: 0, scissors: 0}
+
   def initialize
     @won = 0
     @lost = 0
     @tie = 0
-    @player_choice_log = {rock: 0, paper: 0, scissors: 0 }
     @player_choice = {}
-    @computer_choice = {}
-    strategy = select_strategy
+    @previous_player_choice = {}
+    @strategy = select_strategy
     welcome_message
     @player_input = player_input
   end
 
   def player_input
     loop do
-      puts "Type 'r', 'p' or 's'.\n>"
+      puts "Type 'r', 'p' or 's'.\n"
+
+      computer_choice = strategy_implement(strategy)
       input = gets.chomp.downcase
       if input == 'r'
         value = {rock: 1}
+        player_choice.clear
         player_choice.merge!(value)
         puts "Rock"
-        puts "I chose 'r'. We tied"
-        play_game(player_choice.keys.pop, :rock)
+        # puts "I chose 'r'. We tied"
+        play_game(value.keys.pop, computer_choice)
+        @previous_player_choice = value
         add_player_choice_to_log
 
       elsif input == 'p'
         value = {paper: 1}
+        player_choice.clear
         player_choice.merge!(value)
         puts "Paper"
-        puts "I chose 's'. You lose"
-        play_game(player_choice.keys.pop, :scissors)
+        # puts "I chose 's'. You lose"
+        play_game(value.keys.pop, computer_choice)
         add_player_choice_to_log
+        @previous_player_choice = value
 
       elsif input == 's'
         value = {scissors: 1}
+        player_choice.clear
         player_choice.merge!(value)
         puts "Scissors"
-        puts "I chose 'p'. You win"
-        play_game(player_choice.keys.pop, :paper)
+        # puts "I chose 'p'. You win"
+        play_game(value.keys.pop, computer_choice)
         add_player_choice_to_log
+        @previous_player_choice = value
 
       elsif input == 'exit'
+        if strategy == 1
+          puts "Thank you for playing against the Beat Last Move computer strategy"
+        elsif strategy == 2
+          puts "Thank you for playing against the Beat Player Favorite computer strategy"
+        else
+          puts "Thank you for playing against the Random computer strategy"
+        end
         break
       else
         puts "That is not a valid choice - please try again"
@@ -70,8 +87,8 @@ class Game
 
   def add_player_choice_to_log
     choice = player_choice.keys.pop
-    new_value = @player_choice_log[choice]+1
-    @player_choice_log[choice] = new_value
+    new_value = @@player_choice_log[choice]+1
+    @@player_choice_log[choice] = new_value
   end
 
   def welcome_message
@@ -80,14 +97,26 @@ class Game
   end
 
   def select_strategy
-    strategy_choices = [:beat_last_move, :beat_favorite, :random_computer].sample
-    return strategy_choices
+    strategy_picker = [1, 2, 3].sample
+    return strategy_picker
   end
 
-  def beat_last_move(previous_player_choice)
-    if previous_player_choice == :rock
+  def strategy_implement(strategy_picker)
+    if strategy_picker == 1
+      computer_strategy = beat_last_move
+    elsif strategy_picker == 2
+      computer_strategy = beat_favorite
+    else
+      computer_strategy = random_computer
+    end
+  end
+
+  def beat_last_move
+    if @previous_player_choice == {}
+      computer_choice = [:rock, :paper, :scissors].sample
+    elsif @previous_player_choice.keys.pop == :rock
       computer_choice = :paper
-    elsif previous_player_choice == :paper
+    elsif @previous_player_choice.keys.pop == :paper
       computer_choice = :scissors
     else
       computer_choice = :rock
@@ -95,17 +124,18 @@ class Game
     return computer_choice
   end
 
-  def player_favorite
-    favorite = player_choice_log.max_by{|k,v| v}
-  end
-
-  def beat_favorite(player_favorite)
-    if player_favorite[0] == :rock
-      computer_choice = :paper
-    elsif player_favorite[0] == :paper
-      computer_choice = :scissors
+  def beat_favorite
+    if @@player_choice_log.values == [0,0,0]
+      computer_choice = [:rock, :paper, :scissors].sample
     else
-      computer_choice = :rock
+      favorite = @@player_choice_log.max_by{|k,v| v}
+      if favorite[0] == :rock
+        computer_choice = :paper
+      elsif favorite[0] == :paper
+        computer_choice = :scissors
+      else
+        computer_choice = :rock
+      end
     end
     return computer_choice
   end
